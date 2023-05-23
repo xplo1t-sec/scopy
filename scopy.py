257d445db7968769e90320838ec5aa0830b9cd1b
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2023-05-23 23:42:09 +0530

    implemented parallel processing of URLs using multiple threads for better scaling

--- scopy.py
+++ scopy.py
@@ -3,6 +3,8 @@
 import tldextract
 import argparse
 import sys
+import threading
+from queue import Queue
 
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
@@ -38,6 +40,11 @@ def get_input(input_file=None):
             exit(3)
     return input
 
+def process_url(url, mode, scope, output_queue):
+    scoped = extract_url_components(url, mode=mode)
+    if scoped in scope:
+        output_queue.put(url)
+
 def main():
     parser = argparse.ArgumentParser(description='Filter URLs that match your scope file.')
     parser.add_argument('-s', '--scope', required=True, help='Scope for the URLs')
@@ -45,17 +52,47 @@ def main():
     parser.add_argument('-m', '--mode', default='wildcard', help='Mode to run in (wildcard or fqdn)')
     args = parser.parse_args()
     if args.mode not in ['fqdn', 'wildcard']:
-            print('Invalid mode specified. Mode must be "fqdn" or "wildcard".')
-            return
+        print('Invalid mode specified. Mode must be "fqdn" or "wildcard".')
+        return
     scope = get_scope(args.scope, mode=args.mode)
     urls = get_input(args.input if args.input else None)
     if len(urls) == 0:
         print(f'{RED}[!]{END} {BROWN}Input is empty.{END}')
         return
+
+    output_queue = Queue()
+
+    def worker():
+        while True:
+            url = input_queue.get()
+            if url is None:
+                break
+            process_url(url, args.mode, scope, output_queue)
+            input_queue.task_done()
+
+    num_threads = min(len(urls), threading.cpu_count() * 2)
+    input_queue = Queue()
+    threads = []
+
+    # Create worker threads
+    for _ in range(num_threads):
+        t = threading.Thread(target=worker)
+        t.start()
+        threads.append(t)
+
+    # Enqueue input URLs
     for url in urls:
-        scoped = extract_url_components(url, mode=args.mode)
-        if scoped in scope:
-            print(url)
+        input_queue.put(url)
+
+    input_queue.join()
+
+    for _ in range(num_threads):
+        input_queue.put(None)
+    for t in threads:
+        t.join()
+
+    while not output_queue.empty():
+        print(output_queue.get())
 
 if __name__ == '__main__':
     main()
